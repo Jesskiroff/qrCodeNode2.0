9b64fe5f6baff21df0dd58cc2e7dbddf681a37fc
Jessica Kiroff <jesskiroff@gmail.com>
2024-03-12 21:40:17 -0400

    qr code generator works

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-import inquirer from 'inquirer'
-
-
+import inquirer from 'inquirer';
+import qr from 'qr-image';
+import fs from 'fs';
 inquirer
   .prompt([{
     message:"What URL shall I turn into a QR code for you? ", 
-    name:"URL"
-  }])
+    name:"URL",
+  },
+])
   .then((answers) => {
-    console.log(answers)
+  //  console.log(answers)
+  const url = answers.URL
+  var qr_svg = qr.image(url);
+  qr_svg.pipe(fs.createWriteStream('my_qr_img.png'));
+
+  fs.writeFile('machinereply.txt', url, (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+  }); 
   })
   .catch((error) => {
     if (error.isTtyError) {
@@ -16,3 +25,10 @@ inquirer
       // Something else went wrong
     }
   });
+
+
+
+  // The code below id totally from the qr-image npm module which i've used in my code above, only modified
+  //var qr_svg = qr.image(url /*A const which I created at an earlier step*/); //{ type: 'svg' }I am not using this option bc this module defaults to creating a png image);
+  //qr_svg.pipe(fs.createWriteStream('my_qr_img.png'));
+  
